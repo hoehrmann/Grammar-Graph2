1b5a50c1d7b59aed92fb687f735424fdba7ebcca
bjoern <bjoern@hoehrmann.de>
2018-03-03 01:31:23 +0100

    topology

--- lib/Grammar/Graph2/TestParser.pm
+++ lib/Grammar/Graph2/TestParser.pm
@@ -12,6 +12,11 @@ use Grammar::Graph2::TestParser::MatchEnumerator;
 use List::Util qw/min max/;
 use Graph::SomeUtils qw/:all/;
 use Grammar::Graph2::Automata;
+use List::OrderBy;
+use List::StackBy;
+
+# Mixins
+
 
 has 'input_path' => (
   is       => 'ro',
@@ -94,7 +99,7 @@ sub BUILD {
           'Fi', 'Fi1', 'Fi2') THEN 1 END
       ) AS is_pop,
       MAX(CASE WHEN an = 'self_loop' THEN av END)   AS self_loop,
-      MAX(CASE WHEN an = 'epsilon_key' THEN av END)   AS epsilon_key,
+      MAX(CASE WHEN an = 'topo' THEN av END)   AS topo,
       MAX(CASE WHEN an = 'epsilon_group' THEN av END)   AS epsilon_group
           
     FROM
@@ -313,6 +318,12 @@ sub BUILD {
 
   $self->_replace_conditionals();
 
+  $self->_rematerialise_all_views();
+
+  $self->_add_self_loop_attributes();
+
+  $self->_rematerialise_all_views();
+
   $self->_add_topological_attributes();
   
   $self->_rematerialise_all_views();
@@ -337,7 +348,93 @@ sub _add_self_loop_attributes {
     for @self_loop;
 }
 
-sub _add_topological_attributes {
+sub _scg_topological_depth {
+  my ($d) = @_;
+
+  my $scg = $d->strongly_connected_graph;
+  my $scgf = Graph::Feather->new(
+    vertices => [ $scg->vertices ],
+    edges => [ $scg->edges ],
+  );
+
+  my $result = $scgf->{dbh}->selectall_hashref(q{
+    WITH RECURSIVE
+    roots(vertex) AS (
+      SELECT DISTINCT
+        v.vertex_name AS vertex
+      FROM
+        vertex v
+          LEFT JOIN Edge successors
+            ON (successors.src = v.vertex_name)
+          LEFT JOIN Edge predecessors
+            ON (predecessors.dst = v.vertex_name)
+      WHERE
+        predecessors.src IS NULL
+    ),
+    dfs(vertex, depth) AS (
+      SELECT
+        vertex,
+        0 AS depth
+      FROM roots
+      UNION ALL
+      SELECT
+        e.dst AS vertex,
+        depth + 1 AS depth
+      FROM dfs
+        INNER JOIN Edge e
+          ON (e.src = dfs.vertex)
+    ),
+    topology(vertex, max_depth, min_depth) AS (
+      SELECT vertex, MAX(depth), MIN(depth) FROM dfs
+      GROUP BY vertex
+    )
+    SELECT * FROM topology ORDER BY max_depth DESC
+  }, 'vertex');
+
+  for my $k (keys %$result) {
+    next unless $k =~ /\+/;
+    for my $v (split/\+/, $k) {
+      # FIXME: clone?
+      $result->{$v} = $result->{$k};
+    }
+  }
+
+  return $result;
+}
+
+sub _topo_parent_child {
+  my ($self) = @_;
+
+  my $d = Graph::Directed->new;
+
+  $d->add_vertices(
+    map { @$_ } $self->_dbh->selectall_array(q{
+      SELECT
+        src_p.vertex
+      FROM
+        m_vertex_property src_p
+    })
+  );
+
+  $d->add_edges(
+    $self->_dbh->selectall_array(q{
+      SELECT
+        pc.parent AS parent,
+        pc.child AS child
+      FROM
+        view_parent_child pc
+          INNER JOIN m_vertex_property parent_p
+            ON (pc.parent = parent_p.vertex)
+          INNER JOIN m_vertex_property child_p
+            ON (pc.child = child_p.vertex)
+
+    })
+  );
+
+  return _scg_topological_depth($d);
+}
+
+sub _topo_epsilon {
   my ($self) = @_;
 
   my $d = Graph::Directed->new;
@@ -371,57 +468,63 @@ sub _add_topological_attributes {
     edges => [ $scg->edges ],
   );
 
-  my @order = $scgf->{dbh}->selectall_array(q{
-    WITH RECURSIVE
-    roots(vertex) AS (
-      SELECT DISTINCT
-        v.vertex_name AS vertex
-      FROM
-        vertex v
-          LEFT JOIN Edge successors
-            ON (successors.src = v.vertex_name)
-          LEFT JOIN Edge predecessors
-            ON (predecessors.dst = v.vertex_name)
-      WHERE
-        predecessors.src IS NULL
-    ),
-    dfs(vertex, depth) AS (
-      SELECT
-        vertex,
-        0 AS depth
-      FROM roots
-      UNION ALL
-      SELECT
-        e.dst AS vertex,
-        depth + 1 AS depth
-      FROM dfs
-        INNER JOIN Edge e
-          ON (e.src = dfs.vertex)
-    ),
-    topology(vertex, max_depth, min_depth) AS (
-      SELECT vertex, MAX(depth), MIN(depth) FROM dfs
-      GROUP BY vertex
-    )
-    SELECT * FROM topology ORDER BY max_depth DESC
-  });
+  return _scg_topological_depth($d)
+}
+
+sub _add_topological_attributes {
+  my ($self) = @_;
 
   my $json = JSON->new
     ->canonical(1)
     ->ascii(1)
     ->indent(0);
 
-  while (@order) {
-    my $current = shift @order;
-    $self->g->g->set_vertex_attribute($_, 'epsilon_key', $current->[1])
-      for split/\+/, $current->[0];
+  my $t1 = $self->_topo_epsilon();
+  my $t2 = $self->_topo_parent_child();
 
-#    next unless $current =~ /\+/;
+=pod
 
+
+use YAML::XS;
+warn Dump {
+  t1 => $t1,
+  t2 => $t2,
+  vertices => [ $self->_dbh->selectall_array(q{ SELECT * FROM m_vertex_property }) ],
+};
+
+=cut
+
+  for ($self->g->g->vertices) {
+    warn $_ unless defined $t1->{$_}{vertex};
     $self->g->g->set_vertex_attribute($_, 'epsilon_group',
-      $json->encode([ split/\+/, $current->[0]])
-    ) for split/\+/, $current->[0];
-
+      $json->encode([ split/\+/, $t1->{$_}{vertex}])
+    );
   }
+
+  my @order = order_by_desc {
+    $t1->{$_}{max_depth} // warn
+  } then_by_desc {
+    $t2->{$_}{max_depth} // warn
+  } then_by_desc {
+    $t1->{$_}{min_depth} // warn
+  } then_by_desc {
+    $t2->{$_}{min_depth} // warn
+  } $self->g->g->vertices();
+
+  my @stacks = stack_by {
+    join ',', $t1->{$_}{max_depth},
+              $t1->{$_}{min_depth},
+              $t2->{$_}{max_depth},
+              $t2->{$_}{min_depth}
+  } @order;
+
+  while (@stacks) {
+    my $current = shift @stacks;
+    $self->g->g->set_vertex_attribute($_,
+      'topo', 2 + $#stacks)
+        for @$current
+  }
+
 }
 
 # TODO: rename to compute_t or whatever
@@ -640,9 +743,9 @@ FROM
       ON (dst_each.vertex = dst_p.vertex)
 ORDER BY
   src_pos,
-  src_p.epsilon_key,
+  src_p.topo,
   dst_pos,
-  dst_p.epsilon_key
+  dst_p.topo
 
 
     
